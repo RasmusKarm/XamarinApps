8639c0249229985c4146bdb39312194ede9b1ee6
priitmat <priitmat@gmail.com>
2018-09-27 09:29:23 +0300

    core

---
+++ WeatherApp/WeatherApp.Core/Core.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            string key = "a519d2565f58343b5f157d056e658aca";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=London";
+
+            dynamic results = await DataService.GetDataFromService(queryString).ConfigureAwait(false);
+            var weather = new Weather();
+            weather.Temperature = (string)results["main"]["temp"] + " C";
+            return weather;
+        }
+    }
+}
