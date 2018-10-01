104252847e138f803d4cc5fefed1ea12d8d73af5
priitmat <priitmat@gmail.com>
2018-10-01 08:27:52 +0300

    weather

--- WeatherApp/WeatherApp/MainActivity.cs
+++ WeatherApp/WeatherApp/MainActivity.cs
@@ -3,17 +3,29 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using WeatherApp.Core;
 
 namespace WeatherApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected  override  void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+
+            button.Click += Button_Click;
+
+        }
+
+        private async void Button_Click(object sender, System.EventArgs e)
+        {
+            var weather = await Core.Core.GetWeather("asd");
         }
     }
 }
